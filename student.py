ed52b37f688436ab7287dfd252a8efa9ccac786b
akkamma-rabbanagoudra <akkammarabbanagoudra@gmail.com>
2025-11-22 09:28:55 +0530

    student.py

---
+++ student.py
@@ -0,0 +1,9 @@
+import sys
+if len(sys.argv) !=3:
+    print("Usage: python student.py <name">
+script_name=sys.argv[1]
+name=sys.argv[1]
+rollno=sys.argv[2]
+print("Script Name:",script_name)
+print("Student Name:",name)
+print("Roll Number:",rollno)
